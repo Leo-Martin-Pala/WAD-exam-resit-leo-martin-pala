c6b32683b80f613a7b9d1e2e7d570c3cd5c0be95
leo-martin <example@mail.com>
2025-01-30 12:03:01 +0200

    task 6 complete

--- src/views/Instructor.vue
+++ src/views/Instructor.vue
@@ -1,24 +1,34 @@
 <template>
   <div>
-  <h1>Requests Management Pannel</h1>
-  <div class="container">
-    <table>
-      <thead> 
-          <tr> 
-            <th>St. Code</th>
-            <th>Course name</th>
-            <th>Course code</th>
-            <th>St. request</th>
-            <th>Decision</th>
-            <th>Decision jusification</th>
-          </tr>
-      </thead> 
-      <tbody> 
-
-      </tbody> 
-    </table>
+    <h1>Requests Management Pannel</h1>
+    <div class="container">
+      <table>
+        <thead>
+        <tr>
+          <th>St. Code</th>
+          <th>Course name</th>
+          <th>Course code</th>
+          <th>St. request</th>
+          <th>Decision</th>
+          <th>Decision justification</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr class="item" v-for="strequest in strequests" :key="strequest.id">
+          <td>{{ strequest.studentcode }}</td>
+          <td>{{ strequest.coursename }}</td>
+          <td>{{ strequest.coursecode }}</td>
+          <td>{{ strequest.studentrequest }}</td>
+          <td><input type="text" id="decision"></td>
+          <td><input type="text" id="decision-justification"></td>
+          <td>
+            <button class="descision">Descision</button>
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
-</div>
 </template>
 
 
@@ -33,26 +43,29 @@ export default {
   methods: {
     fetchRecords() {
       fetch(`http://localhost:3000/api/requests`)
-        .then((response) => response.json())
-        .then((data) => (this.strequests = data))
-        .catch((err) => console.log(err.message));
-  },
+          .then((response) => response.json())
+          .then((data) => (this.strequests = data))
+          .catch((err) => console.log(err.message));
+    },
   },
   mounted() {
     this.fetchRecords();
     console.log("mounted");
-  } 
+  }
 };
 </script>
 
 <style scoped>
+
 h1 {
   font-size: 20px;
 }
+
 h3 {
   text-align: center;
   color: rgb(8, 110, 110);
 }
+
 .container {
   background: #d5d7d8;
   box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
@@ -62,5 +75,24 @@ h3 {
   width: 90%;
   display: flex;
   justify-content: center;
+}
+
+table tr {
+  background-color: grey;
+}
+
+table th, .table td {
+  border: 1px solid #ddd;
+  padding: 8px;
+  text-align: left;
+}
+
+table th {
+  background-color: yellowgreen;
+}
+
+input {
+  width: 80%;
+  padding: 5px;
 }
 </style>
